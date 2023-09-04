984c83f187d74b842480ed8fe3059412d9a442f8
Mechanic707 <2584740196@qq.com>
2023-09-04 14:18:56 +0800

    test

--- python/package_test/main.py
+++ python/package_test/main.py
@@ -2,7 +2,10 @@
 import src_test as st
 
 handle = st.ZMCWrapper()
+st.connect(handle)
+#st.set_parameter(handle, 6, 5)
 
+st.src_move(handle, 6, 5, 10)
 #handle.func_test()
 
-st.test(handle)
+#st.test(handle)
